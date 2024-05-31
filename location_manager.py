c4aeb6e10e013337c5a33a6b1c3e77397b18a05d
IgorDiamandi <161752477+IgorDiamandi@users.noreply.github.com>
2024-05-31 16:59:49 +0300

    Removed external library for location management and replaced it with a json.
    Added a combo box for city selection

---
+++ location_manager.py
@@ -0,0 +1,49 @@
+import timezonefinder as tf
+import pytz
+from datetime import datetime
+from geopy.geocoders import Nominatim
+
+
+class LocationManager:
+    def __init__(self, city, country, longitude, latitude):
+        self.city = city
+        self.country = country
+        self.longitude = longitude
+        self.latitude = latitude
+
+    def get_timezone(self):
+        geolocator = Nominatim(user_agent="WeatherChecker")
+        location = geolocator.geocode(self.city)
+
+        if location:
+            timezone_str = tf.TimezoneFinder().timezone_at(lng=float(self.longitude), lat=float(self.latitude))
+            if timezone_str:
+                return timezone_str
+            else:
+                return "Time zone not found for this city"
+        else:
+            return "Incorrect city input"
+
+
+def display_date_time(user_timezone, location_timezone=None):
+      user_time = datetime.now(pytz.timezone(user_timezone))
+      if location_timezone:
+        location_time = user_time.astimezone(pytz.timezone(location_timezone))
+        formatted_location_time = location_time.strftime("%A, %B %d, %Y, %I:%M %p")
+        return f"Date and time in {location_timezone}: {formatted_location_time}"
+
+
+def extract_city_name(city_with_country):
+    if '(' in city_with_country and ')' in city_with_country:
+        pos = city_with_country.index('(')
+        city_name = city_with_country[:pos].strip()
+        return city_name
+    else:
+        return city_with_country
+
+
+def find_city_by_name(city_name, data):
+    for city_node in data:
+        if city_node['name'] == city_name:
+            return city_node
+    return None
--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import streamlit as st
+import json
 from datetime import datetime
-from date_time_manager import get_timezone_from_city, display_date_time, get_latitude_and_longitude
+from location_manager import LocationManager, display_date_time, extract_city_name, find_city_by_name
 from openweathermap_manager import OpenWeatherMapManager
 
-user_input = st.text_input("Please enter a city name:")
+with open('unique_cities.json', 'r', encoding='UTF-8') as f:
+    cities_data = json.load(f)
+
+city_names = {}
+for city in cities_data:
+    city_name = city['name']
+    country_code = city['country']
+    if city_name in city_names:
+        city_names[city_name].append(country_code)
+    else:
+        city_names[city_name] = [country_code]
+
+cities = []
+for city_name, countries in city_names.items():
+    if len(countries) > 1:
+        for country in countries:
+            cities.append(f"{city_name} ({country})")
+    else:
+        cities.append(city_name)
+
+
+user_input = st.selectbox("Select a city:", options=cities)
+
 
 if user_input:
-    timezone = get_timezone_from_city(user_input)
+    city_name = extract_city_name(user_input)
+    location = LocationManager(
+        find_city_by_name(city_name, cities_data)['name'],
+        find_city_by_name(city_name, cities_data)['country'],
+        find_city_by_name(city_name, cities_data)['lat'],
+        find_city_by_name(city_name, cities_data)['lng'])
+
+    timezone = location.get_timezone()
+
     st.write(f"Timezone: {timezone}")
     st.write(f"Current Date and Time in {user_input}: {display_date_time(timezone, timezone)}")
 
-    latitude, longitude = get_latitude_and_longitude(user_input)
-    openweathermap_manager = OpenWeatherMapManager(latitude, longitude, datetime.now())
+    openweathermap_manager = OpenWeatherMapManager(location.latitude, location.longitude, datetime.now())
     openweathermap_manager.publish_weather_data()
-
-
